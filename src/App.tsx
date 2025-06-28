5534d2e1c9eb63a31dd2d68fde3166245cbcc706
Jay Bond <jaybond@mac.com>
2025-06-28 13:35:21 +0100

    Add CSS files and update components for deployment

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,34 @@
-// src/App.tsx
-import React, { useState } from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import HomeView from './components/HomeView';
-import CreateVostcardStep1 from './components/CreateVostcardStep1';
-import CreateVostcardStep2 from './components/CreateVostcardStep2';
-import CreateVostcardStep3 from './components/CreateVostcardStep3';
-import ScrollingCameraView from './components/ScrollingCameraView';
-import CameraView from './components/CameraView';
-import ListView from './components/ListView';
-import LandingPage from './components/LandingPage';
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import LandingPage from "./pages/LandingPage";
+import LoginPage from "./pages/LoginPage";
+import RegisterPage from "./pages/RegisterPage";
+import HomeView from "./components/HomeView"; // Assuming HomeView is still in components
+import CreateVostcardStep1 from "./components/CreateVostcardStep1";
+import CreateVostcardStep2 from "./components/CreateVostcardStep2";
+import CreateVostcardStep3 from "./components/CreateVostcardStep3";
+import ListView from "./components/ListView";
+import ScrollingCameraView from "./components/ScrollingCameraView";
+import CameraView from "./components/CameraView";
+import { VostcardProvider } from "./context/VostcardContext";
 
 function App() {
-  const [showLanding, setShowLanding] = useState(true);
-
   return (
-    <Router>
-      {showLanding ? (
-        <LandingPage onUnlock={() => setShowLanding(false)} />
-      ) : (
+    <VostcardProvider>
+      <Router>
         <Routes>
-          <Route path="/" element={<HomeView />} />
-          <Route path="/create-step1" element={<CreateVostcardStep1 />} />
-          <Route path="/create-step2" element={<CreateVostcardStep2 />} />
-          <Route path="/create-step3" element={<CreateVostcardStep3 />} />
-          <Route path="/scrolling-camera" element={<ScrollingCameraView />} />
+          <Route path="/" element={<LandingPage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="/register" element={<RegisterPage />} />
+          <Route path="/home" element={<HomeView />} />
+          <Route path="/step1" element={<CreateVostcardStep1 />} />
+          <Route path="/step2" element={<CreateVostcardStep2 />} />
+          <Route path="/step3" element={<CreateVostcardStep3 />} />
+          <Route path="/list" element={<ListView />} />
+          <Route path="/scroll-camera" element={<ScrollingCameraView />} />
           <Route path="/camera" element={<CameraView />} />
-          <Route path="/list-view" element={<ListView />} />
         </Routes>
-      )}
-    </Router>
+      </Router>
+    </VostcardProvider>
   );
 }
 
