f6831e49e4c125b3c0b2374f942771e4fca8e4a0
Jay Bond <jaybond@mac.com>
2025-06-30 10:51:18 +0100

    Ready for Netlify deploy

---
+++ src/pages/AccountSettingsView.tsx
@@ -0,0 +1,15 @@
+
+
+import React from 'react';
+
+const AccountSettingsView = () => {
+  return (
+    <div style={{ padding: '20px' }}>
+      <h1>Account Settings</h1>
+      <p>Manage your account preferences here.</p>
+      {/* Add more account-related settings or actions here */}
+    </div>
+  );
+};
+
+export default AccountSettingsView;
