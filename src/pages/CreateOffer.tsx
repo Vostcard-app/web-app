896003899a334dc51d7bb9cddcbabba504f17b9e
Jay Bond <jaybond@mac.com>
2025-07-02 17:21:13 +0100

    Ready for Netlify deploy

---
+++ src/pages/CreateOffer.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function CreateOffer() {
+  return (
+    <div>
+      <h2>Create Offer Page</h2>
+      {/* Add your offer creation form here */}
+    </div>
+  );
+} 
