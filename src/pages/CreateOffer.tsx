eb31f8c18d1a75d66451b96e39744b7cb67c99be
Jay Bond <jaybond@mac.com>
2025-07-02 22:37:51 +0100

    Ready for Netlify deploy

--- src/pages/CreateOffer.tsx
+++ src/pages/CreateOffer.tsx
@@ -1,10 +1,5 @@
 import React from "react";
 
 export default function CreateOffer() {
-  return (
-    <div>
-      <h2>Create Offer Page</h2>
-      {/* Add your offer creation form here */}
-    </div>
-  );
+  return <div>Create Offer Page</div>;
 } 
