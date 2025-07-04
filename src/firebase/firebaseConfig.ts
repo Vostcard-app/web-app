34cc95bf8468cfba617a4c7b024b190bf36f2402
Jay Bond <jaybond@mac.com>
2025-07-04 22:12:31 +0100

    Ready for Netlify deploy

--- src/firebase/firebaseConfig.ts
+++ src/firebase/firebaseConfig.ts
@@ -9,7 +9,7 @@ const firebaseConfig = {
   apiKey: "AIzaSyDNbo5KseD17nVAGw2jvGykVbelgJoIpFo",
   authDomain: "vostcard-a3b71.firebaseapp.com",
   projectId: "vostcard-a3b71",
-  storageBucket: "vostcard-a3b71.appspot.com",  // ✅ Fixed this line
+  storageBucket: "vostcard-a3b71.firebasestorage.app",  // ✅ Fixed this line
   messagingSenderId: "897191018962",
   appId: "1:897191018962:web:f989f240aa68cd7099080b",
   measurementId: "G-G6BVKFH8BE"
