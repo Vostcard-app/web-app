80230c884d813cd813f486580e7bfbe4956bcb36
Jay Bond <jaybond@mac.com>
2025-07-09 18:23:13 +0100

    Ready for Netlify deploy

--- src/pages/PinPlacerTool.tsx
+++ src/pages/PinPlacerTool.tsx
@@ -8,11 +8,14 @@ import { FaArrowLeft, FaCheck, FaTimes, FaMapMarkerAlt, FaCrosshairs } from 'rea
 import 'leaflet/dist/leaflet.css';
 
 // Fix for default markers in react-leaflet
-delete (Icon.Default.prototype as any)._getIconUrl;
-Icon.Default.mergeOptions({
-  iconRetinaUrl: require('leaflet/dist/images/marker-icon-2x.png'),
-  iconUrl: require('leaflet/dist/images/marker-icon.png'),
-  shadowUrl: require('leaflet/dist/images/marker-shadow.png'),
+const defaultIcon = new Icon({
+  iconUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon.png',
+  iconRetinaUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-icon-2x.png',
+  shadowUrl: 'https://cdnjs.cloudflare.com/ajax/libs/leaflet/1.7.1/images/marker-shadow.png',
+  iconSize: [25, 41],
+  iconAnchor: [12, 41],
+  popupAnchor: [1, -34],
+  shadowSize: [41, 41]
 });
 
 interface PinData {
@@ -248,6 +251,7 @@ const PinPlacerTool: React.FC<PinPlacerToolProps> = ({ pinData }) => {
           <Marker
             position={pinPosition}
             ref={markerRef}
+            icon={defaultIcon}
             draggable={true}
             eventHandlers={{
               dragstart: () => {
