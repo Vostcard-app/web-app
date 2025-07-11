66f7e81b0528fdd8fe8aa771ec75f9184fa0f856
Jay Bond <jaybond@mac.com>
2025-07-11 09:37:42 +0100

    Ready for Netlify deploy

--- src/pages/CreateVostcardStep2.tsx
+++ src/pages/CreateVostcardStep2.tsx
@@ -121,7 +121,7 @@ export default function CreateVostcardStep2() {
         display: 'flex',
         flexDirection: 'column',
         alignItems: 'center',
-        padding: '15px 20px 0 20px',
+        padding: '30px 20px 0 20px',
         boxSizing: 'border-box',
         maxHeight: 'calc(100vh - 120px)',
         overflowY: 'auto'
