2fb02921385945198590a0b6a8314b59f9410d03
Jay Bond <jaybond@mac.com>
2025-07-11 09:17:23 +0100

    Ready for Netlify deploy

--- src/pages/VostcardDetailView.tsx
+++ src/pages/VostcardDetailView.tsx
@@ -278,9 +278,7 @@ const VostcardDetailView: React.FC = () => {
       createdAt = String(vostcard.createdAt);
     }
   }
-git add .
-git commit -m "Ready for Netlify deploy"
-git push origin main
+
   return (
     <div
       style={{
