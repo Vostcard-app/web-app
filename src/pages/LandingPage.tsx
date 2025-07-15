3934db754de8b0700b41955cbd4254804aeca801
Jay Bond <jaybond@mac.com>
2025-07-15 12:14:22 +0100

    Ready for Netlify deploy

--- src/pages/LandingPage.tsx
+++ src/pages/LandingPage.tsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect, useRef } from "react";
-import { useState, useEffect, useRef } from "react";
 import { useNavigate } from "react-router-dom";
 import "./LandingPage.css";
 
