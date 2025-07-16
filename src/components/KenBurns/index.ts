8d7d0d2deaa96ccac36d5d4dc6e5bff93f9cd697
Jay Bond <jaybond@mac.com>
2025-07-16 16:30:26 +0100

    Ready for Netlify deploy

--- src/components/KenBurns/index.ts
+++
@@ -1,4 +0,0 @@
-export { default as KenBurnsControls } from './KenBurnsControls';
-export { default as KenBurnsCanvas } from './KenBurnsCanvas';
-export { KenBurnsProcessor } from './KenBurnsProcessor';
-export type { KenBurnsConfig, KenBurnsPhoto, KenBurnsResult, KenBurnsError, KenBurnsStatus } from './types'; 
