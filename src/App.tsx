57e9e3b1d660f19f2f05c9e8ea25d5d1439f790a
Jay Bond <jaybond@mac.com>
2025-07-21 00:07:12 +0100

    Ready for Netlify deploy

--- src/App.tsx
+++ src/App.tsx
@@ -27,44 +27,40 @@ import Step2CameraView from "./pages/Step2CameraView";
 import SavedVostcardsListView from "./components/SavedVostcardsListView";
 import SettingsView from "./pages/SettingsView";
 import AccountSettingsView from "./pages/AccountSettingsView";
-
-import MyPostedVostcardsListView from "./pages/MyPostedVostcardsListView";
+import UserSettingsView from "./pages/UserSettingsView";
+import VostcardDetailView from "./pages/VostcardDetailView";
+import PinPlacerTool from "./pages/PinPlacerTool";
+import DrivecardPinPlacer from "./pages/DrivecardPinPlacer";
 import MyVostcardListView from "./pages/MyVostcardListView";
+import MyPostedVostcardsListView from "./pages/MyPostedVostcardsListView";
+import AllPostedVostcardsView from "./pages/AllPostedVostcardsView";
+import LikedVostcardsView from "./pages/LikedVostcardsView";
+import FollowingView from "./pages/FollowingView";
+import FriendListView from "./pages/FriendListView";
+import VostboxView from "./pages/VostboxView";
+import QuickcardListView from "./pages/QuickcardListView";
+import QuickcardDetailView from "./pages/QuickcardDetailView";
+import ScriptLibraryView from "./pages/ScriptLibraryView";
+import ScriptEditorView from "./pages/ScriptEditorView";
+import ScriptToolView from "./pages/ScriptToolView";
+import UserProfileView from "./pages/UserProfileView";
+import BusinessProfileView from "./pages/BusinessProfileView";
 import AdvertiserPortal from "./pages/AdvertiserPortal";
-import CreateOfferView from './pages/CreateOfferView';
-import AllPostedVostcardsView from './pages/AllPostedVostcardsView';
-import EditStoreProfileView from './pages/EditStoreProfileView';
-import VostcardDetailView from './pages/VostcardDetailView';
-import OfferView from './pages/OfferView';
-import OffersListView from './pages/OffersListView';
-import ScriptLibraryView from './pages/ScriptLibraryView';
-import ScriptEditorView from './pages/ScriptEditorView';
-import ScriptToolView from './pages/ScriptToolView';
-import LikedVostcardsView from './pages/LikedVostcardsView';
-import FollowingView from './pages/FollowingView';
-import UserProfileView from './pages/UserProfileView';
-import UserSettingsView from './pages/UserSettingsView';
-import SuggestionBoxView from './pages/SuggestionBoxView';
-import FlagFormView from './pages/FlagFormView';
-import ReportBugView from './pages/ReportBugView';
-import PinPlacerTool from './pages/PinPlacerTool';
-import DrivecardPinPlacer from './pages/DrivecardPinPlacer';
-import { AuthRedirect } from './components/AuthRedirect';
-import PublicVostcardView from './pages/PublicVostcardView';
-import PublicQuickcardView from './pages/PublicQuickcardView';
-import EmailVostcardView from './pages/EmailVostcardView';
-import AdminPanel from './pages/AdminPanel';
-import FriendListView from './pages/FriendListView';
-import VostboxView from './pages/VostboxView';
-import QuickcardListView from './pages/QuickcardListView';
-import QuickcardDetailView from './pages/QuickcardDetailView';
-import VostcardCameraView from "./pages/VostcardCameraView";
-import VostcardStudioView from './pages/VostcardStudioView';
-import DrivecardsListView from './pages/DrivecardsListView';
-import SharedVostcardView from './pages/SharedVostcardView';
-import SharedQuickcardView from './pages/SharedQuickcardView';
-import BusinessProfileView from './pages/BusinessProfileView';
-import ExampleView from './pages/ExampleView';
+import CreateOfferView from "./pages/CreateOfferView";
+import EditStoreProfileView from "./pages/EditStoreProfileView";
+import OfferView from "./pages/OfferView";
+import OffersListView from "./pages/OffersListView";
+import AdminPanel from "./pages/AdminPanel";
+import ReportBugView from "./pages/ReportBugView";
+import FlagFormView from "./pages/FlagFormView";
+import SuggestionBoxView from "./pages/SuggestionBoxView";
+import DrivecardsListView from "./pages/DrivecardsListView";
+import VostcardStudioView from "./pages/VostcardStudioView";
+import ExampleView from "./pages/ExampleView";
+import PublicVostcardView from "./pages/PublicVostcardView";
+import PublicQuickcardView from "./pages/PublicQuickcardView";
+import EmailVostcardView from "./pages/EmailVostcardView";
+import AuthRedirect from "./components/AuthRedirect";
 
 function App() {
   return (
@@ -74,96 +70,96 @@ function App() {
           <FollowingProvider>
             <DriveModeProvider>
               <Router>
-                <AuthRedirect />
-                <ResponsiveContainer>
-                  <Routes>
-                    {/* 🔑 Authentication */}
-                    <Route path="/" element={<RootView />} />
-                    <Route path="/landing" element={<LandingPage />} />
-                    <Route path="/login" element={<LoginPage />} />
-                    <Route path="/register" element={<RegistrationPage />} />
-                    <Route path="/user-guide" element={<UserGuideView />} />
+              <AuthRedirect />
+              <ResponsiveContainer>
+                <Routes>
+                  {/* 🔑 Authentication */}
+                  <Route path="/" element={<RootView />} />
+                  <Route path="/landing" element={<LandingPage />} />
+                  <Route path="/login" element={<LoginPage />} />
+                  <Route path="/register" element={<RegistrationPage />} />
+                  <Route path="/user-guide" element={<UserGuideView />} />
 
-                    {/*  Public Access */}
-                    <Route path="/share/:id" element={<PublicVostcardView />} />
-                    <Route path="/share-quickcard/:id" element={<PublicQuickcardView />} />
-                    <Route path="/email/:id" element={<EmailVostcardView />} />
-                    <Route path="/public-map" element={<PublicHomeView />} />
+                  {/*  Public Access */}
+                  <Route path="/share/:id" element={<PublicVostcardView />} />
+                  <Route path="/share-quickcard/:id" element={<PublicQuickcardView />} />
+                  <Route path="/email/:id" element={<EmailVostcardView />} />
+                  <Route path="/public-map" element={<PublicHomeView />} />
 
-                    {/* 🏠 Main */}
-                    <Route path="/home" element={<HomeView />} />
-                    <Route path="/browse-area" element={<BrowseAreaView />} />
-                    <Route path="/list" element={<ListView />} />
-                    <Route path="/all-posted-vostcards" element={<AllPostedVostcardsView />} />
-                    <Route path="/offers-list" element={<OffersListView />} />
+                  {/* 🏠 Main */}
+                  <Route path="/home" element={<HomeView />} />
+                  <Route path="/browse-area" element={<BrowseAreaView />} />
+                  <Route path="/list" element={<ListView />} />
+                  <Route path="/all-posted-vostcards" element={<AllPostedVostcardsView />} />
+                  <Route path="/offers-list" element={<OffersListView />} />
+    
+                  <Route path="/my-posted-vostcards" element={<MyPostedVostcardsListView />} />
+                  <Route path="/edit-my-vostcards" element={<MyVostcardListView />} />
+                  <Route path="/liked-vostcards" element={<LikedVostcardsView />} />
+                  <Route path="/following" element={<FollowingView />} />
+                  <Route path="/friends" element={<FriendListView />} />
+                  <Route path="/vostbox" element={<VostboxView />} />
 
-                    <Route path="/my-posted-vostcards" element={<MyPostedVostcardsListView />} />
-                    <Route path="/edit-my-vostcards" element={<MyVostcardListView />} />
-                    <Route path="/liked-vostcards" element={<LikedVostcardsView />} />
-                    <Route path="/following" element={<FollowingView />} />
-                    <Route path="/friends" element={<FriendListView />} />
-                    <Route path="/vostbox" element={<VostboxView />} />
+                  {/* 📱 Quickcard Routes */}
+                  <Route path="/quickcards" element={<QuickcardListView />} />
+                  <Route path="/quickcard/:id" element={<QuickcardDetailView />} />
 
-                    {/* 📱 Quickcard Routes */}
-                    <Route path="/quickcards" element={<QuickcardListView />} />
-                    <Route path="/quickcard/:id" element={<QuickcardDetailView />} />
+                  {/* 📜 Script Management */}
+                  <Route path="/scripts" element={<ScriptLibraryView />} />
+                  <Route path="/script-editor" element={<ScriptEditorView />} />
+                  <Route path="/script-tool" element={<ScriptToolView />} />
 
-                    {/* 📜 Script Management */}
-                    <Route path="/scripts" element={<ScriptLibraryView />} />
-                    <Route path="/script-editor" element={<ScriptEditorView />} />
-                    <Route path="/script-tool" element={<ScriptToolView />} />
+                  {/* 📹 Recording */}
+                  <Route path="/camera" element={<CameraView />} />
+                  <Route path="/scrolling-camera" element={<ScrollingCameraView />} />
+                  <Route path="/quickcard-camera" element={<QuickcardCameraView />} />
 
-                    {/* 📹 Recording */}
-                    <Route path="/camera" element={<CameraView />} />
-                    <Route path="/scrolling-camera" element={<ScrollingCameraView />} />
-                    <Route path="/quickcard-camera" element={<QuickcardCameraView />} />
+                  {/* 🛠️ Creation Flow */}
+                  <Route path="/create-step1" element={<CreateVostcardStep1 />} />
+                  <Route path="/create-step2" element={<CreateVostcardStep2 />} />
+                  <Route path="/create-step3" element={<CreateVostcardStep3 />} />
+                  <Route path="/quickcard-step3" element={<QuickcardStep3 />} />
+                  <Route path="/step2-camera" element={<Step2CameraView />} />
 
-                    {/* 🛠️ Creation Flow */}
-                    <Route path="/create-step1" element={<CreateVostcardStep1 />} />
-                    <Route path="/create-step2" element={<CreateVostcardStep2 />} />
-                    <Route path="/create-step3" element={<CreateVostcardStep3 />} />
-                    <Route path="/quickcard-step3" element={<QuickcardStep3 />} />
-                    <Route path="/step2-camera" element={<Step2CameraView />} />
+                  {/* 📍 Location */}
+                  <Route path="/pin-placer" element={<PinPlacerTool />} />
+                  <Route path="/drivecard-pin-placer" element={<DrivecardPinPlacer />} />
 
-                    {/* 📍 Location */}
-                    <Route path="/pin-placer" element={<PinPlacerTool />} />
-                    <Route path="/drivecard-pin-placer" element={<DrivecardPinPlacer />} />
+                  {/* 🎯 Details */}
+                  <Route path="/vostcard/:id" element={<VostcardDetailView />} />
+                  <Route path="/offer/:id" element={<OfferView />} />
 
-                    {/* 🎯 Details */}
-                    <Route path="/vostcard/:id" element={<VostcardDetailView />} />
-                    <Route path="/offer/:id" element={<OfferView />} />
+                  {/* 👤 Profile */}
+                  <Route path="/user-settings" element={<UserSettingsView />} />
+                  <Route path="/settings" element={<SettingsView />} />
+                  <Route path="/account-settings" element={<AccountSettingsView />} />
+                  <Route path="/user-profile/:userId" element={<UserProfileView />} />
+                  <Route path="/business-profile/:businessId" element={<BusinessProfileView />} />
 
-                    {/* 👤 Profile */}
-                    <Route path="/user-settings" element={<UserSettingsView />} />
-                    <Route path="/settings" element={<SettingsView />} />
-                    <Route path="/account-settings" element={<AccountSettingsView />} />
-                    <Route path="/user-profile/:userId" element={<UserProfileView />} />
-                    <Route path="/business-profile/:businessId" element={<BusinessProfileView />} />
+                  {/* 🏢 Business */}
+                  <Route path="/advertiser-portal" element={<AdvertiserPortal />} />
+                  <Route path="/create-offer" element={<CreateOfferView />} />
+                  <Route path="/edit-store-profile" element={<EditStoreProfileView />} />
 
-                    {/* 🏢 Business */}
-                    <Route path="/advertiser-portal" element={<AdvertiserPortal />} />
-                    <Route path="/create-offer" element={<CreateOfferView />} />
-                    <Route path="/edit-store-profile" element={<EditStoreProfileView />} />
+                  {/* 🛡️ Admin */}
+                  <Route path="/admin" element={<AdminPanel />} />
 
-                    {/* 🛡️ Admin */}
-                    <Route path="/admin" element={<AdminPanel />} />
+                  {/* 🐛 Support */}
+                  <Route path="/report-bug" element={<ReportBugView />} />
+                  <Route path="/flag-form" element={<FlagFormView />} />
+                  <Route path="/suggestion-box" element={<SuggestionBoxView />} />
 
-                    {/* 🐛 Support */}
-                    <Route path="/report-bug" element={<ReportBugView />} />
-                    <Route path="/flag-form" element={<FlagFormView />} />
-                    <Route path="/suggestion-box" element={<SuggestionBoxView />} />
+                  {/* 🏎️ Drive Mode */}
+                  <Route path="/drivecards" element={<DrivecardsListView />} />
 
-                    {/* 🏎️ Drive Mode */}
-                    <Route path="/drivecards" element={<DrivecardsListView />} />
+                  {/* 🎬 Studio */}
+                  <Route path="/studio" element={<VostcardStudioView />} />
 
-                    {/* 🎬 Studio */}
-                    <Route path="/studio" element={<VostcardStudioView />} />
-
-                    {/* 🔍 Examples */}
-                    <Route path="/example" element={<ExampleView />} />
-                  </Routes>
-                </ResponsiveContainer>
-              </Router>
+                  {/* 🔍 Examples */}
+                  <Route path="/example" element={<ExampleView />} />
+                </Routes>
+              </ResponsiveContainer>
+            </Router>
             </DriveModeProvider>
           </FollowingProvider>
         </ScriptProvider>
--- src/pages/LoginPage.tsx
+++ src/pages/LoginPage.tsx
@@ -3,9 +3,10 @@ import { useNavigate } from "react-router-dom";
 import { signInWithEmailAndPassword } from "firebase/auth";
 import { collection, query, where, getDocs } from "firebase/firestore";
 import { auth, db } from "../firebase/firebaseConfig";
-import "./LoginPage.css"; // RESTORE ORIGINAL CSS
+import { useResponsive } from "../hooks/useResponsive";
 
 export default function LoginPage() {
+  const { isDesktop } = useResponsive();
   const [usernameOrEmail, setUsernameOrEmail] = useState("");
   const [password, setPassword] = useState("");
   const [showPassword, setShowPassword] = useState(false);
@@ -23,170 +24,19 @@ export default function LoginPage() {
   const getEmailFromUsername = async (username: string): Promise<string | null> => {
     try {
       const trimmedUsername = username.trim();
+      console.log('🔍 Looking up username:', { original: username, trimmed: trimmedUsername });
+      
+      // Try exact match first
       const exactQuery = query(collection(db, "users"), where("username", "==", trimmedUsername));
       let querySnapshot = await getDocs(exactQuery);
       
       if (!querySnapshot.empty) {
         const userDoc = querySnapshot.docs[0];
         const email = userDoc.data().email;
+        console.log('✅ Found exact match:', { username: trimmedUsername, email });
         return email || null;
       }
       
-      const allUsersQuery = query(collection(db, "users"));
-      const allUsersSnapshot = await getDocs(allUsersQuery);
-      
-      const lowerUsername = trimmedUsername.toLowerCase();
-      for (const userDoc of allUsersSnapshot.docs) {
-        const userData = userDoc.data();
-        const storedUsername = userData.username;
-        
-        if (storedUsername && storedUsername.toLowerCase() === lowerUsername) {
-          const email = userData.email;
-          return email || null;
-        }
-      }
-      
-      return null;
-    } catch (error) {
-      console.error("Error looking up username:", error);
-      return null;
-    }
-  };
-
-  const handleLogin = async (e?: React.FormEvent) => {
-    if (e) {
-      e.preventDefault();
-    }
-
-    if (!usernameOrEmail || !password) {
-      setError("Please fill in all fields");
-      return;
-    }
-
-    setLoading(true);
-    setError("");
-
-    try {
-      const trimmedInput = usernameOrEmail.trim();
-      let emailToUse = trimmedInput;
-      
-      if (!isEmail(trimmedInput)) {
-        const foundEmail = await getEmailFromUsername(trimmedInput);
-        
-        if (!foundEmail) {
-          setError("No account found with this username.");
-          setLoading(false);
-          return;
-        }
-        
-        emailToUse = foundEmail;
-      }
-
-      await signInWithEmailAndPassword(auth, emailToUse, password);
-    } catch (err: any) {
-      let errorMessage = "Failed to log in. Please check your credentials.";
-      
-      if (err.code) {
-        switch (err.code) {
-          case 'auth/user-not-found':
-            errorMessage = "No account found with this email address or username.";
-            break;
-          case 'auth/wrong-password':
-            errorMessage = "Incorrect password. Please try again.";
-            break;
-          case 'auth/invalid-email':
-            errorMessage = "Invalid email address format.";
-            break;
-          case 'auth/too-many-requests':
-            errorMessage = "Too many failed login attempts. Please try again later.";
-            break;
-          default:
-            errorMessage = `Login error: ${err.message || err.code}`;
-        }
-      }
-      
-      setError(errorMessage);
-    } finally {
-      setLoading(false);
-    }
-  };
-
-  return (
-    <div className="login-container">
-      {/* Header */}
-      <div className="header">
-        <h1 onClick={() => navigate('/')}>Vōstcard</h1>
-      </div>
-
-      {/* Content */}
-      <div style={{ padding: '20px' }}>
-        <h1 className="welcome">Welcome</h1>
-
-        {error && (
-          <div style={{
-            color: 'red',
-            marginBottom: '20px',
-            textAlign: 'center',
-            padding: '10px',
-            backgroundColor: '#ffe6e6',
-            borderRadius: '8px'
-          }}>
-            {error}
-          </div>
-        )}
-
-        <form onSubmit={handleLogin}>
-          {/* Username or Email Input */}
-          <input
-            type="text"
-            placeholder="Username or Email"
-            value={usernameOrEmail}
-            onChange={(e) => setUsernameOrEmail(e.target.value)}
-            className="input-field"
-          />
-
-          {/* Password Input */}
-          <div className="password-wrapper">
-            <input
-              type={showPassword ? "text" : "password"}
-              placeholder="Password"
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
-              className="input-field"
-            />
-            <button
-              type="button"
-              onClick={() => setShowPassword(!showPassword)}
-              className="eye-button"
-            >
-              <i className={`fas ${showPassword ? "fa-eye-slash" : "fa-eye"}`}></i>
-            </button>
-          </div>
-
-          {/* Log In Button */}
-          <button
-            type="submit"
-            disabled={loading}
-            className="main-button"
-          >
-            {loading ? 'Logging in...' : 'Log In'}
-          </button>
-
-          {/* Register Section */}
-          <p className="register-prompt">
-            If you don't have an account tap here
-          </p>
-
-          {/* Register Button */}
-          <button
-            type="button"
-            onClick={() => navigate("/register")}
-            className="main-button"
-          >
-            Register
-          </button>
-        </form>
-      </div>
-    </div>
-  );
-}
+      // If no exact match, try case-insensitive search by getting all users and filtering
+      console.log('🔍 No exact match, trying case-insensitive search...');
+      const
