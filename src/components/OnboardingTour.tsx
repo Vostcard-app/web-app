3d710cc5b1ac063c16529968e493e3161744f53f
Jay Bond <jaybond@mac.com>
2025-08-04 20:10:56 +0100

    Add 10px top margin to 'Need Help?' title on Quick Guide page 3
    - Changed title margin from '0 0 16px 0' to '10px 0 16px 0'
    - Creates 10px spacing above the title for better visual separation
    - Improves overall layout spacing of page 3

--- src/components/OnboardingTour.tsx
+++ src/components/OnboardingTour.tsx
@@ -351,7 +351,7 @@ const OnboardingTour: React.FC<OnboardingTourProps> = React.memo(({
 
             {/* Title */}
             <h2 style={{
-              margin: '0 0 16px 0',
+              margin: '10px 0 16px 0',
               fontSize: '28px',
               fontWeight: '700',
               color: '#002B4D',
