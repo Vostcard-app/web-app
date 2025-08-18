eabb97f61c3a297b09be543bd20dafa90bb95a37
jaynbond <info@vostcard.com>
2025-08-17 20:18:43 -0700

    fix: add visibility field to all posted vostcards queries and indexes

--- src/pages/AllPostedVostcardsView.tsx
+++ src/pages/AllPostedVostcardsView.tsx
@@ -369,6 +369,7 @@ const AllPostedVostcardsView: React.FC = () => {
           q1 = query(
             collection(db, 'vostcards'), 
             where('state', '==', 'posted'),
+            where('visibility', '==', 'public'),
             where('userRole', '==', 'guide'),
             limit(ITEMS_PER_PAGE)
           );
@@ -377,6 +378,7 @@ const AllPostedVostcardsView: React.FC = () => {
           q1 = query(
             collection(db, 'vostcards'), 
             where('state', '==', 'posted'),
+            where('visibility', '==', 'public'),
             limit(ITEMS_PER_PAGE)
           );
         }
@@ -475,6 +477,7 @@ const AllPostedVostcardsView: React.FC = () => {
         q1 = query(
           collection(db, 'vostcards'), 
           where('state', '==', 'posted'),
+          where('visibility', '==', 'public'),
           where('userRole', '==', 'guide'),
           startAfter(lastDoc),
           limit(ITEMS_PER_PAGE)
@@ -484,6 +487,7 @@ const AllPostedVostcardsView: React.FC = () => {
         q1 = query(
           collection(db, 'vostcards'), 
           where('state', '==', 'posted'),
+          where('visibility', '==', 'public'),
           startAfter(lastDoc),
           limit(ITEMS_PER_PAGE)
         );
