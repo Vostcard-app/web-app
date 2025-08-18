bb3c710b2ce94467d2f321998a47ede645b667f7
jaynbond <info@vostcard.com>
2025-08-17 18:10:58 -0700

    fix: update flag name to TEMP_UNIFIED_VOSTCARD_FLOW

--- src/pages/HomeView.tsx
+++ src/pages/HomeView.tsx
@@ -25,7 +25,7 @@ import VostcardPin from '../assets/Vostcard_pin.png';
 import OfferPin from '../assets/Offer_pin.png';
 import QuickcardPin from '../assets/quickcard_pin.png';
 import { AVAILABLE_CATEGORIES, AVAILABLE_TYPES } from '../types/VostcardTypes';
-import { UNIFIED_VOSTCARD_FLOW } from '../utils/flags';
+import { TEMP_UNIFIED_VOSTCARD_FLOW } from '../utils/flags';
 
 // FIXED: Import pin images from assets folder for better Leaflet compatibility
 const vostcardIcon = new L.Icon({
@@ -1194,7 +1194,7 @@ const HomeView = () => {
   const handleCreateClick = (e: React.MouseEvent) => {
     e.preventDefault();
     clearVostcard();
-    navigate(UNIFIED_VOSTCARD_FLOW ? '/create/step1' : '/create-step1');
+    navigate(TEMP_UNIFIED_VOSTCARD_FLOW ? '/create/step1' : '/create-step1');
   };
 
   const handleCreateQuickcard = (e: React.MouseEvent) => {
